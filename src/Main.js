7edac22aff82fff1b5490ca35bb833fb5fb92bed
Yausuhiro Katayama <yasucom0406@gmail.com>
2019-01-05 08:40:33 -0800

    Import reset.css and common.css

--- src/Main.js
+++ src/Main.js
@@ -4,17 +4,18 @@ import {
     NavLink,
     BrowserRouter,
 } from "react-router-dom";
-import Home from "./Home";
-import About from "./About";
-import Works from "./Works";
-import Navigation from "./Navigation";
+import Home from "./home/Home";
+import About from "./about/About";
+import Works from "./works/Works";
+import Navigation from "./common/Navigation";
+import './common/reset.css';
+import './common/common.css';
 
 class Main extends Component {
     render() {
         return (
             <BrowserRouter>
                 <div>
-                    <h1>Single Page Application</h1>
                     <Navigation />
                     <div className="content">
                         <Route exact path="/" component={Home}/>
