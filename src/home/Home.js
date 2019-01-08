85cbce95d53f94285373f163c367c04220d3b7dc
Yausuhiro Katayama <yasucom0406@gmail.com>
2019-01-08 14:58:14 -0800

    Added Navlink

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import Anime from 'react-anime';
+import {
+    NavLink,
+} from "react-router-dom";
 import './home.css';
 class Home extends Component {
     constructor() {
@@ -12,43 +15,56 @@ class Home extends Component {
         return (
             <div>
                 <div className="main-visual">
-                    <div className="left">
-                        <div>
-                            <Anime easing="easeOutElastic"
-                                   duration={1000}
-                                   direction="delay"
-                                   delay={500}
-                                   translateX={['-500%',0]}>
-                                <h1>HELLO</h1>
-                            </Anime>
-                            <ul>
-                                <Anime easing="easeOutElastic"
+                    <Anime easing="easeInOutQuad"
+                           duration={1000}
+                           direction="delay"
+                           delay={100}
+                           opacity={[0,1]}
+                           translateX={['-500%',0]}>
+                        <div className="left">
+                            <div>
+                                <Anime easing="easeInOutQuad"
                                        duration={1000}
                                        direction="delay"
-                                       delay={1000}
+                                       delay={500}
                                        translateX={['-500%',0]}>
-                                    <li>I’m Yasuhiro.<br/>Welcome to my website.</li>
-                                    <li>I’m Front End developer.<span>I love creative</span></li>
+                                    <h1>HELLO</h1>
                                 </Anime>
-                                <li>
-                                    <Anime easing="easeOutElastic"
+                                <ul>
+                                    <Anime easing="easeInOutQuad"
                                            duration={1000}
                                            direction="delay"
-                                           delay={1500}
+                                           delay={1000}
                                            translateX={['-500%',0]}>
-                                        <ul className="sns">
-                                            <li><a href="" className="fa fa-linkedin"></a></li>
-                                            <li><a href="" className="fa fa-github"></a></li>
-                                            <li><a href="" className="fa fa-facebook"></a></li>
-                                        </ul>
+                                        <li>I’m Yasuhiro.<br/>Welcome to my website.</li>
+                                        <li>I’m Front End developer.<span>I love creative</span></li>
                                     </Anime>
-                                </li>
-                            </ul>
+                                    <li>
+                                        <Anime easing="easeInOutQuad"
+                                               duration={1000}
+                                               direction="delay"
+                                               delay={1500}
+                                               translateX={['-500%',0]}>
+                                            <ul className="sns">
+                                              <li><a href="https://www.linkedin.com/in/yasuhiro-katayama-118423160/" target="_blank" className="fa fa-linkedin"></a></li>
+                                              <li><a href="https://github.com/yasu0406" target="_blank" className="fa fa-github"></a></li>
+                                              <li><a href="https://www.facebook.com/yasuhiro.katayama2" target="_blank" className="fa fa-facebook"></a></li>
+                                            </ul>
+                                        </Anime>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
-                    <div className="right">
+                    </Anime>
 
-                    </div>
+                    <Anime easing="easeInOutQuad"
+                           duration={1000}
+                           direction="delay"
+                           delay={100}
+                           translateX={['500%',0]}>
+                        <div className="right">
+                        </div>
+                    </Anime>
                 </div>
                 <section>
                     <ul>
@@ -58,12 +74,13 @@ class Home extends Component {
                                     <ul>
                                         <li>Android App</li>
                                         <li><h2>Project Name</h2></li>
-                                        <li><button className="btn">View Project</button></li>
+                                        <li><NavLink to="/worksDetail" className="btn">View Project</NavLink></li>
                                     </ul>
                                 </li>
                                 <li><img src="/img/img-works-01.png" alt=""/></li>
                                 <li>TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextText</li>
                             </ul>
+                            <div className="color-box"></div>
                         </li>
                         <li>
                             <ul className="container">
@@ -71,12 +88,13 @@ class Home extends Component {
                                     <ul>
                                         <li>Android App</li>
                                         <li><h2>Project Name</h2></li>
-                                        <li><button className="btn">View Project</button></li>
+                                        <li><NavLink to="/worksDetail" className="btn">View Project</NavLink></li>
                                     </ul>
                                 </li>
                                 <li><img src="/img/img-works-01.png" alt=""/></li>
                                 <li>TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextText</li>
                             </ul>
+                            <div className="color-box"></div>
                         </li>
                         <li>
                             <ul className="container">
@@ -84,12 +102,13 @@ class Home extends Component {
                                     <ul>
                                         <li>Android App</li>
                                         <li><h2>Project Name</h2></li>
-                                        <li><button className="btn">View Project</button></li>
+                                        <li><NavLink to="/worksDetail" className="btn">View Project</NavLink></li>
                                     </ul>
                                 </li>
                                 <li><img src="/img/img-works-01.png" alt=""/></li>
                                 <li>TextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextTextText</li>
                             </ul>
+                            <div className="color-box"></div>
                         </li>
                     </ul>
                 </section>
