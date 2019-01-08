1bea8985fa91eeb89892229241dc4d9dd03b16f3
Yausuhiro Katayama <yasucom0406@gmail.com>
2019-01-08 14:52:58 -0800

    Refacted and Added sns icons

--- src/common/Navigation.js
+++ src/common/Navigation.js
@@ -12,7 +12,7 @@ class Navigation extends Component {
     }
 
     remove() {
-        if(this.state.addClass == true) {
+        if(this.state.addClass) {
             this.setState({addClass: false});
         }
     }
@@ -20,28 +20,34 @@ class Navigation extends Component {
     render() {
         let activeNav = 'button_container ';
         let openNav = 'overlay ';
-        if(this.state.addClass == true) {
-            activeNav += 'active'
-            openNav += 'open'
+        if(this.state.addClass) {
+            activeNav += 'active';
+            openNav += 'open';
         }
         return (
             <div onClick={this.remove.bind(this)}>
                 <div className={activeNav} id="toggle" onClick={this.toggle.bind(this)}>
-                    <span class="top"></span>
-                    <span class="middle"></span>
-                    <span class="bottom"></span>
+                    <span className="top"></span>
+                    <span className="middle"></span>
+                    <span className="bottom"></span>
                 </div>
                 <div className={openNav} id="overlay">
-                    <nav class="overlay-menu">
+                    <nav className="overlay-menu">
                         <ul>
                             <li><NavLink to="/">Home</NavLink></li>
                             <li><NavLink to="/about">About</NavLink></li>
                             <li><NavLink to="/works">Works</NavLink></li>
+                            <li>
+                                <ul className="sns">
+                                    <li><a href="https://www.linkedin.com/in/yasuhiro-katayama-118423160/" target="_blank" className="fa fa-linkedin"></a></li>
+                                    <li><a href="https://github.com/yasu0406" target="_blank" className="fa fa-github"></a></li>
+                                    <li><a href="https://www.facebook.com/yasuhiro.katayama2" target="_blank" className="fa fa-facebook"></a></li>
+                                </ul>
+                            </li>
                         </ul>
                     </nav>
                 </div>
             </div>
-
         );
     }
 }
