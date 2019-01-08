7b67c7e409495c0f164d69683312d8eca12a7234
Yausuhiro Katayama <yasucom0406@gmail.com>
2019-01-08 15:03:32 -0800

    Created worksDetail.js

---
+++ src/worksDetail/WorksDetail.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class WorksDetail extends Component {
+    render() {
+        return (
+            <div className="works-detail">
+                <p>test</p>
+            </div>
+        );
+    }
+}
+
+export default WorksDetail;
