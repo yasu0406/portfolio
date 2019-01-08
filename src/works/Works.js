09217cbd9d2ae02c86307427c84d2c5d4bbe9dd6
Yausuhiro Katayama <yasucom0406@gmail.com>
2019-01-08 14:55:08 -0800

    Added NavLink

--- src/works/Works.js
+++ src/works/Works.js
@@ -1,11 +1,95 @@
 import React, { Component } from "react";
+import {
+    NavLink,
+} from "react-router-dom";
+import './works.css';
 
 class Works extends Component {
     render() {
         return (
-            <div>
-                <h2>Works</h2>
-                <p>aaaaaaa</p>
+            <div className="works">
+                <ul>
+                    <li>
+                        <NavLink to="/worksDetail">
+                            <ul>
+                                <li><h2><span>Android</span>ProjectName</h2></li>
+                                <li></li>
+                            </ul>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/worksDetail">
+                            <ul>
+                                <li></li>
+                                <li><h2><span>Android</span>ProjectName</h2></li>
+                            </ul>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/worksDetail">
+                            <ul>
+                                <li><h2><span>Android</span>ProjectName</h2></li>
+                                <li></li>
+                            </ul>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/worksDetail">
+                            <ul>
+                                <li></li>
+                                <li><h2><span>Android</span>ProjectName</h2></li>
+                            </ul>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/worksDetail">
+                            <ul>
+                                <li><h2><span>Android</span>ProjectName</h2></li>
+                                <li></li>
+                            </ul>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/worksDetail">
+                            <ul>
+                                <li></li>
+                                <li><h2><span>Android</span>ProjectName</h2></li>
+                            </ul>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/worksDetail">
+                            <ul>
+                                <li><h2><span>Android</span>ProjectName</h2></li>
+                                <li></li>
+                            </ul>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/worksDetail">
+                            <ul>
+                                <li></li>
+                                <li><h2><span>Android</span>ProjectName</h2></li>
+                            </ul>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/worksDetail">
+                            <ul>
+                                <li><h2><span>Android</span>ProjectName</h2></li>
+                                <li></li>
+                            </ul>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/worksDetail">
+                            <ul>
+                                <li></li>
+                                <li><h2><span>Android</span>ProjectName</h2></li>
+                            </ul>
+                        </NavLink>
+                    </li>
+                </ul>
             </div>
         );
     }
