e946d844fabfbbbef4867072cc1c19b84d5a19a6
Yausuhiro Katayama <yasucom0406@gmail.com>
2019-03-22 11:41:14 -0700

    Fixed html tag to About.js

--- src/about/About.js
+++ src/about/About.js
@@ -9,24 +9,25 @@ class About extends Component {
         return (
                 <div className="about">
                     <section className="my-name container">
-                        <div className="left">
-                            <h1>Yasuhiro<span className="sp-line-break title-span">Katayama.</span><span className="line-break">Front End developer.</span></h1>
+                        <div className="left" data-delighter>
+                            <h1 className="fade-up">Yasuhiro<span className="sp-line-break title-span">Katayama.</span><span className="line-break fade-up">Front End developer.</span></h1>
                         </div>
                         <div className="right">
                             <img src="/img/img-profile.png" alt=""/>
+                            <div className="blue" data-delighter></div>
                         </div>
                     </section>
 
                     <section className="skill">
                         <div className="container">
-                            <div>
-                                <div className="left">
+                            <div data-delighter>
+                                <div className="left fade-up">
                                     <h2>Skills</h2>
                                 </div>
                                 <div className="right">
                                     <ul>
                                         <li>
-                                            <ul>
+                                            <ul data-delighter>
                                                 <li><h3>Programing</h3></li>
                                                 <li>- Javascript</li>
                                                 <li>- React & React Native</li>
@@ -37,7 +38,7 @@ class About extends Component {
                                             </ul>
                                         </li>
                                         <li>
-                                            <ul>
+                                            <ul data-delighter>
                                                 <li><h3>Design</h3></li>
                                                 <li>- Sketch</li>
                                                 <li>- XD</li>
@@ -48,11 +49,11 @@ class About extends Component {
                                     </ul>
                                 </div>
                             </div>
-                            <div>
-                                <div className="left">
+                            <div  data-delighter>
+                                <div className="left fade-up">
                                     <h2>Policy</h2>
                                 </div>
-                                <div className="right">
+                                <div className="right" data-delighter>
                                     <p>I am currently studying at Cornerstone International Community College of Canada (CICCC) for Web and Mobile Development program.
                                         In Japan, I worked as a frontend engineer for two years and backend engineer for one year and 6 months. I am working volunteer at the web company and a freelance job.</p>
                                 </div>
@@ -61,27 +62,27 @@ class About extends Component {
                     </section>
                     <section className="about-me">
                         <div className="container">
-                            <div>
-                                <div className="left">
+                            <div data-delighter>
+                                <div className="left fade-up">
                                     <h2>I am a…</h2>
                                 </div>
-                                <div className="right">
+                                <div className="right" data-delighter>
                                     <p>I am currently studying at Cornerstone International Community College of Canada (CICCC) for Web and Mobile Development program.
                                         In Japan, I worked as a frontend engineer for two years and backend engineer for one year and 6 months. I am working volunteer at the web company and a freelance job.</p>
                                 </div>
                             </div>
-                            <div>
-                                <div className="left">
+                            <div data-delighter>
+                                <div className="left fade-up">
                                     <h2>Discography</h2>
                                 </div>
                                 <div className="right">
-                                    <dl>
-                                        <dt>2016-2017</dt>
-                                        <dd>AGENTGROW Inc in Tokyo</dd>
-                                        <dt>2015-2016</dt>
-                                        <dd>PENSEUR Inc in Osaka</dd>
-                                        <dt>2014-2015</dt>
-                                        <dd>MITELI Ltd in Osaka</dd>
+                                    <dl data-delighter>
+                                        <dt className="fade-up">2016-2017</dt>
+                                        <dd className="fade-up">AGENTGROW Inc in Tokyo<a href="https://www.agent-grow.com/"></a></dd>
+                                        <dt className="fade-up">2015-2016</dt>
+                                        <dd className="fade-up">PENSEUR Inc in Osaka<a href="https://www.penseur.co.jp/"></a></dd>
+                                        <dt className="fade-up">2014-2015</dt>
+                                        <dd className="fade-up">MITELI Ltd in Osaka<a href="http://www.miteli.co.jp/"></a></dd>
                                     </dl>
                                 </div>
                             </div>
