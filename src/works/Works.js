609a9caf1f78f7ed61132afbbdaa6d76b45f5346
Yausuhiro Katayama <yasucom0406@gmail.com>
2019-03-24 17:50:59 -0700

    Changed layout

--- src/works/Works.js
+++ src/works/Works.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import {
     NavLink,
 } from "react-router-dom";
-import './works.scss';
 import delighters from "../common/delighters";
 
 class Works extends Component {
@@ -13,26 +12,58 @@ class Works extends Component {
         return (
             <div className="works container">
                 <ul>
-                    <li>
-                        <h2>01</h2>
-                        <NavLink to="/worksDetail"><img src="/img/thumnail-works-01.png" alt=""/></NavLink>
+                    <li data-delighter>
+                        <NavLink to="/worksDetail">
+                            <img src="/img/img-works-01.png" alt=""/>
+                            <h2 className="fade-up"><NavLink to="/worksDetail">METAL REVISION.INC</NavLink></h2>
+                            <p className="fade-up">Freelance job / Website</p>
+                            <div className="blue" data-delighter></div>
+                            <div className="pop-up"><h3><a href="">View</a></h3></div>
+                        </NavLink>
                     </li>
-                    <li>
-                        <h2>02</h2>
-                        <NavLink to="/worksDetail"><img src="/img/thumnail-works-01.png" alt=""/></NavLink>
+                    <li data-delighter>
+                        <NavLink to="/worksDetail">
+                            <img src="/img/img-works-01.png" alt=""/>
+                            <h2 className="fade-up"><NavLink to="/worksDetail">METAL REVISION.INC</NavLink></h2>
+                            <p className="fade-up">Freelance job / Website</p>
+                            <div className="blue" data-delighter></div>
+                            <div className="pop-up"><h3><a href="">View</a></h3></div>
+                        </NavLink>
                     </li>
-                    <li>
-                        <h2>03</h2>
-                        <NavLink to="/worksDetail"><img src="/img/thumnail-works-01.png" alt=""/></NavLink>
+                    <li data-delighter>
+                        <NavLink to="/worksDetail">
+                            <img src="/img/img-works-01.png" alt=""/>
+                            <h2 className="fade-up"><NavLink to="/worksDetail">METAL REVISION.INC</NavLink></h2>
+                            <p className="fade-up">Freelance job / Website</p>
+                            <div className="blue" data-delighter></div>
+                            <div className="pop-up"><h3><a href="">View</a></h3></div>
+                        </NavLink>
                     </li>
-                    <li>
-                        <h2>04</h2>
-                        <NavLink to="/worksDetail"><img src="/img/thumnail-works-01.png" alt=""/></NavLink>
+                    <li data-delighter>
+                        <NavLink to="/worksDetail">
+                            <img src="/img/img-works-01.png" alt=""/>
+                            <h2 className="fade-up"><NavLink to="/worksDetail">METAL REVISION.INC</NavLink></h2>
+                            <p className="fade-up">Freelance job / Website</p>
+                            <div className="blue" data-delighter></div>
+                            <div className="pop-up"><h3><a href="">View</a></h3></div>
+                        </NavLink>
                     </li>
-                    <li>
-                        <h2>05</h2>
-                        <NavLink to="/worksDetail"><img src="/img/thumnail-works-01.png" alt=""/></NavLink>
+                    <li data-delighter>
+                        <NavLink to="/worksDetail">
+                            <img src="/img/img-works-01.png" alt=""/>
+                            <h2 className="fade-up"><NavLink to="/worksDetail">METAL REVISION.INC</NavLink></h2>
+                            <p className="fade-up">Freelance job / Website</p>
+                            <div className="blue" data-delighter></div>
+                            <div className="pop-up"><h3><a href="">View</a></h3></div>
+                        </NavLink>
                     </li>
+                </ul>
+                <ul className="load-overlay" data-delighter>
+                    <li className="fade-out-up"></li>
+                    <li className="fade-out-up"></li>
+                    <li className="fade-out-up"></li>
+                    <li className="fade-out-up"></li>
+                    <li className="fade-out-up"></li>
                 </ul>
             </div>
         );
