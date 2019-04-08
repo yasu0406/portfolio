7adf5c2d786256a4400553a6c0d9c1dda179b7b6
Yausuhiro Katayama <yasucom0406@gmail.com>
2019-04-08 16:17:41 -0700

    Created handler(), renderNext() and renderList to show work-list, move to next project and manage animation

--- client/src/components/worksDetail/WorksDetail.js
+++ client/src/components/worksDetail/WorksDetail.js
@@ -1,35 +1,76 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import delighters from "../common/delighters";
-import { fetchWork } from "../../actions";
+import { fetchWork, fetchWorks } from "../../actions";
+import {Link} from "react-router-dom";
 
 class WorksDetail extends Component {
+    state = {
+        nextId: 1
+    };
     componentDidMount() {
-        this.props.fetchWork(this.props.match.params.id);
         const promise = new Promise((resolve) => {
-            resolve(this.props.fetchWork(this.props.match.params.id));
+            resolve(this.props.fetchWork(this.props.match.params.id, this.props.fetchWorks()));
         });
         promise.then(() =>
             delighters.init()
         );
-    }
 
+    }
     renderSkills(skillWord) {
         return skillWord.map((skill) => {
             return <li>{skill}</li>
+        })
+    }
+    handler() {
+        var overlay = document.querySelector("#load-overlay")
+        const promise = new Promise((resolve) => {
+            resolve(
+                overlay.classList.remove("started")
+            )
+        });
+        promise.then(() =>
+            delighters.init()
+        )
+    }
+    renderNext() {
+        this.state.nextId =+ this.props.match.params.id + 1;
+        if(this.props.works.length != this.props.match.params.id) {
+            return (
+                <>
+                    <section className="next-project">
+                        <h2><Link onClick={this.handler} to={`/works-detail/${this.state.nextId}`}>Next Project</Link></h2>
+                    </section>
+                </>
+            )
+        }
+    }
+    renderList() {
+        return this.props.works.map((work)  => {
+            return (
+                <>
+                    <li key={work.id} data-delighter>
+                        <div>
+                            <Link to={`/works-detail/${work.id}`}><img src={`/img/${work.thumbnail}`} alt={work.title}/></Link>
+                            <div className="pop-up"><h3><Link onClick={this.handler}  to={`/works-detail/${work.id}`}>View</Link></h3></div>
+                        </div>
+                        <div className="blue" data-delighter></div>
+                    </li>
+                </>
+            )
         })
     }
     render() {
         if (!this.props.work) {
             return <div>Loading...</div>
         }
-        const { title, description, thumbnail, date, category, skills, projectUrl, imgDetailFirst, imgDetailSecond } = this.props.work;
+        const {id, title, description, thumbnail, date, category, skills, projectUrl, imgDetailFirst, imgDetailSecond } = this.props.work;
         var skillWord = skills.split(',');
         return (
             <div className="work-detail">
                 <section className="first-content">
                     <h1>{title}</h1>
-                    <p className="scale-up" data-delighter><img src={`/img/${thumbnail}`} alt={title} /></p>
+                    <p><img src={`/img/${thumbnail}`} alt={title} /></p>
                 </section>
                 <section className="second-content">
                     <div className="container">
@@ -62,18 +103,16 @@ class WorksDetail extends Component {
                         <img src={`/img/${imgDetailSecond}`} alt={title} />
                     </div>
                 </section>
-                <section className="work-list">
-                    <div className="container">
-                        <ul>
-                            <li><a href=""></a></li>
-                            <li><a href=""></a></li>
-                            <li><a href=""></a></li>
-                            <li><a href=""></a></li>
-                            <li><a href=""></a></li>
-                        </ul>
-                    </div>
+                <section>
+                    {this.renderNext()}
                 </section>
-                <ul className="load-overlay" data-delighter>
+                <section className="all-project container">
+                    <h2>All Project</h2>
+                    <ul key={id}>
+                        {this.renderList()}
+                    </ul>
+                </section>
+                <ul id="load-overlay" className="load-overlay" data-delighter>
                     <li className="fade-out-up"></li>
                     <li className="fade-out-up"></li>
                     <li className="fade-out-up"></li>
@@ -87,11 +126,12 @@ class WorksDetail extends Component {
 
 const mapStateToProps = (state, ownProps) => {
     return {
-        work: state.works[ownProps.match.params.id]
+        work: state.works[ownProps.match.params.id],
+        works: Object.values(state.works)
     };
 };
 
 export default connect(
     mapStateToProps,
-    { fetchWork }
+    { fetchWork, fetchWorks }
 )(WorksDetail);
